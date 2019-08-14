6a1f84ade05f5ea50c654f977ccf83d64b839808
Angela Flowers <me@angela.flowers>
2019-08-14 18:08:55 -0400

    fixed zero button styling!

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const NumberButton = props => {
+  function widenZero(digit) {
+    if (digit === "0") {
+      return "240px";
+    } else {
+      return "100px";
+    }
+  }
   return (
-    <button classname={"button-num-" + props.numberValue}>
+    <button classname={"button-num-" + props.numberValue} style={{width: widenZero(props.numberValue)}}>
       {props.numberValue}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
